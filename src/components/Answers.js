5eeb40ec9f10eb53dd585d3f79931dcbd3aff827
Emma Bosworth <emmab3105@gmail.com>
2023-02-04 14:25:38 +0000

    dupicated questions

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,12 +1,18 @@
 import { useState } from "react";
+import data from "../assets/data";
 
 const Answers = ({ question, score, setScore }) => {
 
-  const [correct, setCorrect] = useState(null);
+  const [activeQuestion, setActiveQuestion] = useState(0)
+    const [showScore, setShowScore] = useState(false)
+    const [result, setResult] = useState(0)
+    
+    const [correct, setCorrect] = useState(null);
 
-  const answers = [...question.incorrect_answers, question.correct_answer];
-  answers.sort(() => Math.random() - 0.5);
+    const answers = [...data[activeQuestion].incorrect_answers, data[activeQuestion].correct_answer];
+    answers.sort(() => Math.random() - 0.5);
 
+  
   const handleClick = (e) => {
     if (e.target.innerText === question.correct_answer) {
       setCorrect(true);
@@ -17,17 +23,24 @@ const Answers = ({ question, score, setScore }) => {
   }
 
   return (
-    <div className="answers">
-      {correct === null ?
-        (answers.map((answer) => {
-          return (
-            <button onClick={(e) => handleClick(e)}>{answer}</button>
-          );
-        })) : correct === true || correct === false ? (
-          correct ? <div>Correct!</div> : <div>Incorrect!</div>
-        ) : null}
-    </div>
-  );
+    <div className='quiz'><h1>Quiz</h1>
+    {showScore ? ( <>You Scored {result} out of {data.length}</>
+    ):(
+    <>
+  
+    <div className='answers-section'>
+
+  {correct === null ?
+    (answers.map((answer) => {
+      return (
+        <button onClick={(e) => handleClick(e)}>{answer}</button>
+      );
+    })) : correct === true || correct === false ? (
+      correct ? <div>Correct!</div> : <div>Incorrect!</div>
+    ) : null}
+</div></>)}
+</div>
+);
 }
 
 export default Answers;
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,15 @@
+import React, {useState} from 'react'
+import data from "../assets/data";
 import Answers from "./Answers";
 
 const Question = ({ question, score, setScore }) => {
 
+  const [activeQuestion, setActiveQuestion] = useState(0)
+
   return (
     <div className="question">
-      <h2>{question.question}</h2>
+      <h2>Question {activeQuestion +1} of {data.length}</h2>
+        {data[activeQuestion].question}
       <Answers question={question} score={score} setScore={setScore} />
     </div>
   );
