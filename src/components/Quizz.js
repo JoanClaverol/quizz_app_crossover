a07f708ddfa803b44c7a00a3edc56ab220d9e517
Luis Rojas <luisignacio1210@gmail.com>
2023-02-04 15:05:54 +0100

    adding pagination buttons

--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -3,21 +3,30 @@ import data from "../assets/data";
 import Question from "./Question";
 
 const Quizz = () => {
-
   const [score, setScore] = useState(0);
-
+  const [page, setPage] = useState(1);
   return (
     <div className="quizz">
-      {!data ?
-        <div>Loading...</div> :
+      {!data ? (
+        <div>Loading...</div>
+      ) : (
         data.map((question) => {
-          return <Question question={question} score={score} setScore={setScore} />
-        }
-        )}
-      <h2 className="score">Score {score} / {data.length}</h2>
+          return (
+            <Question question={question} score={score} setScore={setScore} />
+          );
+        })
+      )}
+      <h2 className="score">
+        Score {score} / {data.length}
+      </h2>
+      <>
+        <button disabled={page === 1} onClick={() => setPage(page - 1)}>
+          Previous
+        </button>
+        <button onClick={() => setPage(page + 1)}>Next</button>
+      </>
     </div>
-
   );
-}
+};
 
 export default Quizz;
