ac1d61db1d9f19e6e5f60a745fe714c4499e5346
JoanClaverol <41630699+JoanClaverol@users.noreply.github.com>
2023-02-04 10:45:48 +0100

    cleaned data.js file

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,31 +1,26 @@
 const data = [
   {
-    response_code: 0,
-    results: [
-      {
-        category: "Entertainment: Video Games",
-        type: "multiple",
-        difficulty: "hard",
-        question: "Which of these Generation 1 Pokemon did NOT have an evolution in Generation 4?",
-        correct_answer: "Jynx",
-        incorrect_answers: [
-          "Electabuzz",
-          "Magmar",
-          "Rhydon"
-        ]
-      },
-      {
-        category: "Entertainment: Video Games",
-        type: "multiple",
-        difficulty: "easy",
-        question: "Which Grand Theft Auto (GTA) games have the same setting?",
-        correct_answer: "GTA V and GTA San Andreas",
-        incorrect_answers: [
-          "GTA V and GTA Vice City",
-          "GTA IV and GTA San Andreas",
-          "GTA IV and GTA Vice City"
-        ]
-      }
+    category: "Entertainment: Video Games",
+    type: "multiple",
+    difficulty: "hard",
+    question: "Which of these Generation 1 Pokemon did NOT have an evolution in Generation 4?",
+    correct_answer: "Jynx",
+    incorrect_answers: [
+      "Electabuzz",
+      "Magmar",
+      "Rhydon"
+    ]
+  },
+  {
+    category: "Entertainment: Video Games",
+    type: "multiple",
+    difficulty: "easy",
+    question: "Which Grand Theft Auto (GTA) games have the same setting?",
+    correct_answer: "GTA V and GTA San Andreas",
+    incorrect_answers: [
+      "GTA V and GTA Vice City",
+      "GTA IV and GTA San Andreas",
+      "GTA IV and GTA Vice City"
     ]
   }
 ]
